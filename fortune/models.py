b6c3a999f134ae22d0951b6f80a16e7916dd3613
rerb <bob.erb@aashe.org>
2016-02-27 14:08:11 -0500

    Add Pack.load() and Pack.unload()

--- fortune/models.py
+++ fortune/models.py
@@ -1,10 +1,36 @@
-from django.db import models
+import os
+import sys
+
+from django.db import models, transaction
+
+
+def filename_to_pack_name(filename):
+    return os.path.splitext(
+        os.path.basename(filename))[0].title().replace("_", " ")
 
 
 class Pack(models.Model):
 
     name = models.CharField(max_length=256,
                             unique=True)
+
+    @classmethod
+    def load(cls, pack_filename, pack_name=None):
+        pack_name = (pack_name or filename_to_pack_name(pack_filename))
+
+        with transaction.atomic():
+            pack = cls.objects.create(name=pack_name)
+            with open(pack_filename, 'r') as pack_file:
+                fortunes = pack_file.read()
+                for fortune in [f[:-1] for f in fortunes.split('\n%')]:
+                    if fortune:  # No empty fortunes.
+                        Fortune.objects.create(text=fortune, pack=pack)
+
+    def unload(self, pack_filename=sys.stdout):
+        with open(pack_filename, 'w') as pack_file:
+            for fortune in Fortune.objects.filter(pack=self):
+                pack_file.write(fortune.text + '\n%\n')
+        self.delete()
 
 
 class Fortune(models.Model):
@@ -17,8 +43,8 @@ class Fortune(models.Model):
         return self.text
 
     @classmethod
-    def fortune(self):
-        fortune = Fortune.objects.order_by("?").first()
+    def fortune(cls):
+        fortune = cls.objects.order_by("?").first()
         if fortune:
             return fortune.text
         else:
--- fortune/tests.py
+++ fortune/tests.py
@@ -1,9 +1,97 @@
+import os
+import tempfile
+
 from django.test import TestCase
 
-from .models import Fortune, Pack
+from .models import (Fortune,
+                     Pack,
+                     filename_to_pack_name)
 
 
-class TestFortune(TestCase):
+class TestFilenameToPackNameTestCase(TestCase):
+
+    def test_long_path(self):
+        """Does filename_to_pack_name handle a long path?"""
+        full_path = "/Favorites/chvrches/EveryOpenEye/never_ending_circles.mp3"
+        self.assertEqual("Never Ending Circles",
+                         filename_to_pack_name(full_path))
+
+
+class TestPackTestCase(TestCase):
+
+    fortunes = ["Good luck for you.",
+                "Good luck for me.",
+                "Good luck for everybody."]
+    pack_data = "\n%\n".join(fortunes) + "\n%\n"
+
+    def test_load(self):
+        """Can we load a Pack of Fortunes from a file?"""
+        pack_file = tempfile.NamedTemporaryFile(mode="w",
+                                                delete=False)
+        pack_file.write(self.pack_data)
+        pack_file.close()
+        try:
+            Pack.load(pack_file.name)
+        except:
+            raise
+        finally:
+            os.remove(pack_file.name)
+        self.assertEqual(3, Fortune.objects.count())
+
+    def test_load_named_pack(self):
+        """Can we load a Pack and name it?"""
+        pack_file = tempfile.NamedTemporaryFile(mode="w",
+                                                delete=False)
+        pack_file.write(self.pack_data)
+        pack_file.close()
+        try:
+            Pack.load(pack_file.name, pack_name="Test Fortunes")
+        except:
+            raise
+        finally:
+            os.remove(pack_file.name)
+        pack = Pack.objects.first()
+        self.assertEqual("Test Fortunes", pack.name)
+
+    def test_unload(self):
+        """Can we unload a Pack of Fortunes?"""
+        pack = Pack.objects.create(name="Test Pack")
+        for fortune in self.fortunes:
+            Fortune.objects.create(text=fortune, pack=pack)
+        pack_file = tempfile.NamedTemporaryFile(mode="w")
+        pack.unload(pack_filename=pack_file.name)
+        self.assertEqual(0, Pack.objects.count())
+
+    def test_unload_removes_fortunes(self):
+        """When we unload a Pack of Fortunes, do the Fortunes go away?"""
+        pack = Pack.objects.create(name="Test Pack")
+        for fortune in self.fortunes:
+            Fortune.objects.create(text=fortune, pack=pack)
+        pack_file = tempfile.NamedTemporaryFile(mode="w")
+        pack.unload(pack_filename=pack_file.name)
+        self.assertEqual(0, Fortune.objects.count())
+
+    def test_unload_output(self):
+        """When we unload a Pack of Fortunes, are they saved into a file?"""
+        pack = Pack.objects.create(name="Test Pack")
+        for fortune in self.fortunes:
+            Fortune.objects.create(text=fortune, pack=pack)
+        pack_file = tempfile.NamedTemporaryFile(mode="w", delete=False)
+        pack_filename = pack_file.name
+        try:
+            pack.unload(pack_filename=pack_file.name)
+            pack_file.close()
+            pack_file = open(pack_filename, 'r')
+            lines = pack_file.read()
+        except:
+            raise
+        finally:
+            pack_file.close()
+            os.remove(pack_file.name)
+        self.assertEqual(self.pack_data, lines)
+
+
+class TestFortuneTestCase(TestCase):
 
     def setUp(self):
         self.pack = Pack.objects.create(name="fortunes")
