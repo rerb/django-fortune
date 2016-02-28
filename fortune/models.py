c1078b2971cf52b92d3c8d46407362fa7a47f185
rerb <bob.erb@aashe.org>
2016-02-27 23:38:29 -0500

    Make Fortune unique on Pack + text

--- fortune/models.py
+++ fortune/models.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 from django.db import models, transaction
+from django.db.utils import IntegrityError
 
 
 def filename_to_pack_name(filename):
@@ -17,27 +18,40 @@ class Pack(models.Model):
     @classmethod
     def load(cls, pack_filename, pack_name=None):
         pack_name = (pack_name or filename_to_pack_name(pack_filename))
-
         with transaction.atomic():
-            pack = cls.objects.create(name=pack_name)
+            try:
+                pack = cls.objects.create(name=pack_name)
+            except IntegrityError:
+                print("Pack named {name} already loaded".format(
+                    name=pack_name))
+                return
             with open(pack_filename, 'r') as pack_file:
                 fortunes = pack_file.read()
                 for fortune in [f[:-1] for f in fortunes.split('\n%')]:
                     if fortune:  # No empty fortunes.
                         Fortune.objects.create(text=fortune, pack=pack)
 
-    def unload(self, pack_filename=sys.stdout):
-        with open(pack_filename, 'w') as pack_file:
+    def unload(self, pack_filename=None):
+        try:
+            if pack_filename:
+                pack_file = open(pack_filename, 'w')
+            else:
+                pack_file = sys.stdout
             for fortune in Fortune.objects.filter(pack=self):
                 pack_file.write(fortune.text + '\n%\n')
+        finally:
+            if pack_filename:
+                pack_file.close()
         self.delete()
 
 
 class Fortune(models.Model):
 
-    text = models.CharField(max_length=2046,
-                            unique=True)
+    text = models.CharField(max_length=2046)
     pack = models.ForeignKey(Pack, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('pack', 'text')
 
     def __unicode__(self):
         return self.text
